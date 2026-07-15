fb14a25180413ed7ac66ae490792de1e5f666fad
Lairnan <kamacori123.123@gmail.com>
2026-07-15 17:56:23 +0300

    Add base menu informations from ATMExpress

--- libs/client/core/include/core/menu.hpp
+++ libs/client/core/include/core/menu.hpp
@@ -2,15 +2,17 @@
 #include <stack>
 
 namespace core {
+    struct AMenu {};
+
     struct MenuState {
-        std::stack<MenuInfo> menu_stack;
+        std::stack<AMenu> menu_stack;
     };
 
-    enum MenuInfo {
+    enum MenuInfo : AMenu {
         MainMenu = 0
     };
 
-    enum MainMenuInfo {
+    enum MainMenuInfo : AMenu {
         Exit = 0,
         Settings = 1,
         Products = 2,
@@ -22,5 +24,34 @@ namespace core {
         CardlessCash = 8,
         ManageCards = 9,
         PromotionsAndOffers = 10,
+    };
+
+    enum StartMenuInfo : AMenu {
+        Exit = 0,
+        Authorize = 1,
+        Register = 2
+    };
+
+    enum SettingsMenuInfo : AMenu {
+        Back = 0,
+        AccountInformation = 1,
+        SecuritySettings = 2,
+        ChangeLanguage = 3
+    };
+
+    enum SecurityMenuInfo : AMenu {
+        Back = 0,
+        ChangePassword = 1,
+        ChangeName = 2
+    };
+
+    enum WithdrawMenuInfo : AMenu {
+        Back = 0,
+        SelectCard = 1
+    };
+
+    enum DepositMenuInfo : AMenu {
+        Back = 0,
+        SelectCard = 1
     };
 }
